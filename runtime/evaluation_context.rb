5fb845ee31f1f051d4646e15326c2405c2c17453
Dipesh Gautam <dipeshgtm@gmail.com>
2013-10-28 20:51:48 +0545

    Evaluation context stores keywords in a constant hash

--- runtime/evaluation_context.rb
+++ runtime/evaluation_context.rb
@@ -3,13 +3,22 @@
 # What it really means is the “area” where a given block of code is evaluated to determine how it should be handled and what it should do.
 # One of the things such an environment or frame will do is determine the current class that any code is operating on. 
 # So I need a mechanism to provide this evaluation context.
-
+require 'pry'
 class EvaluationContext
 	attr_accessor :current_self, :current_klass
 
+	@@constants = {}
 	def initialize(current_self)
 		@current_self = current_self
 		@current_klass = current_self.klass
+	end
+
+	def [](name)
+		@@constants[name]
+	end
+
+	def []=(name,value)
+		@@constants[name] = value
 	end
 end
 
