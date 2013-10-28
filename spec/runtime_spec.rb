15f33308c9c10bd62f5c9720c8595559f61e0e40
Dipesh Gautam <dipeshgtm@gmail.com>
2013-10-28 20:52:47 +0545

    Runtime["Class"] should have klass Runtime["Class"]

--- spec/runtime_spec.rb
+++ spec/runtime_spec.rb
@@ -1,7 +1,11 @@
+require_relative '../runtime/object'
+require_relative '../runtime/evaluation_context'
+require_relative '../runtime/loader'
+require 'pry'
 class RuntimeTest
   describe 'Testing the Runtime' do
     it 'should determine that a class is a class' do
-      Runtime["Class"].runtime_class.should == Runtime["Class"]
+      Runtime["Class"].klass.should == Runtime["Class"]
     end
   end
 end # class: RuntimeTest
